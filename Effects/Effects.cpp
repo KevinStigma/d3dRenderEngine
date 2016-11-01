dcf8d3a83c096445c596c9f0100d8ec6353fecb2
kevinstigma <781422447@qq.com>
2016-11-01 20:53:12 +0800

    Effect cpp files

---
+++ Effects/Effects.cpp
@@ -0,0 +1,73 @@
+#include "Effects.h"
+
+#pragma region Effect
+Effect::Effect(ID3D11Device* device, const std::wstring& filename)
+	: mFX(0)
+{
+	DWORD shaderFlags = 0;
+#if defined( DEBUG ) || defined( _DEBUG )
+	shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+
+	ID3D10Blob* compiledShader = 0;
+	ID3D10Blob* compilationMsgs = 0;
+	HRESULT hr = D3DX11CompileFromFile(filename.c_str(), 0, 0, 0, "fx_5_0", shaderFlags,
+		0, 0, &compiledShader, &compilationMsgs, 0);
+
+	if (compilationMsgs != 0)
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+		ReleaseCOM(compilationMsgs);
+	}
+	if (FAILED(hr))
+	{
+		DXTrace(__FILE__, (DWORD)__LINE__, hr, L"D3DX11CompileFromFile", true);
+	}
+
+	HR(D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(),
+		0, device, &mFX));
+
+	ReleaseCOM(compiledShader);
+}
+
+Effect::~Effect()
+{
+	ReleaseCOM(mFX);
+}
+#pragma endregion
+
+#pragma region BasicEffect
+BasicEffect::BasicEffect(ID3D11Device* device, const std::wstring& filename)
+	: Effect(device, filename)
+{
+	Light1Tech        = mFX->GetTechniqueByName("Light1");
+	Light2Tech        = mFX->GetTechniqueByName("Light2");
+	Light3Tech        = mFX->GetTechniqueByName("Light3");
+	WorldViewProj     = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+	World             = mFX->GetVariableByName("gWorld")->AsMatrix();
+	WorldInvTranspose = mFX->GetVariableByName("gWorldInvTranspose")->AsMatrix();
+	EyePosW           = mFX->GetVariableByName("gEyePosW")->AsVector();
+	DirLights         = mFX->GetVariableByName("gDirLights");
+	Mat               = mFX->GetVariableByName("gMaterial");
+}
+
+BasicEffect::~BasicEffect()
+{
+}
+#pragma endregion
+
+#pragma region Effects
+
+BasicEffect* Effects::BasicFX = 0;
+
+void Effects::InitAll(ID3D11Device* device)
+{
+	BasicFX = new BasicEffect(device, L"FX/Basic.fx");
+}
+
+void Effects::DestroyAll()
+{
+	SafeDelete(BasicFX);
+}
+#pragma endregion
